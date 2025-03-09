089abbab9b8a345046411bb995fbde29ac685583
LilitDemirchyan <demirchyanlilit125@gmail.com>
2025-03-10 01:24:06 +0400

    1.cs

---
+++ task7/1.cs
@@ -0,0 +1,23 @@
+// using System;
+
+// class Person{
+//     public string FirstName { get; set; }
+//     public string LastName { get; set; }
+//     private readonly string fullName;
+//     public string FullName => fullName;
+//     public Person(string firstName, string lastName){
+//         FirstName = firstName;
+//         LastName = lastName;
+//         fullName = FirstName + " " + LastName;
+//     }
+// }
+
+// class Mainprog{
+//     static void Main()
+//     {
+//         Person person = new Person("Ellen", "Roze");
+//         Console.WriteLine(person.FullName); 
+//         person.FirstName = "Jane";
+//         Console.WriteLine(person.FullName);
+//     }
+// }
